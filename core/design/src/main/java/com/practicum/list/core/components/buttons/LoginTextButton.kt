60d755671cfc0d8d446fe0bcbb26592a3d67d9bf
valtherys <melikhovainna@gmail.com>
2026-07-02 14:44:06 +0300

    feat: 52 создала эскиз LoginScreen
    # Conflicts:
    #	feature/auth/src/main/res/values/strings.xml

---
+++ core/design/src/main/java/com/practicum/list/core/components/buttons/LoginTextButton.kt
@@ -0,0 +1,54 @@
+package com.practicum.list.core.components.buttons
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.practicum.list.core.theme.R
+import com.practicum.list.core.theme.ShoppingListTheme
+
+@Composable
+fun LoginTextButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    buttonText: String
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed = interactionSource.collectIsPressedAsState().value
+
+    val textColor = if (isPressed) {
+        MaterialTheme.colorScheme.primary.copy(alpha = 0.6f)
+    } else {
+        MaterialTheme.colorScheme.primary
+    }
+    TextButton(
+        modifier = modifier.fillMaxWidth(),
+        onClick = onClick,
+        enabled = true,
+        interactionSource = interactionSource,
+        colors = ButtonDefaults.textButtonColors(
+            containerColor = Color.Transparent,
+            contentColor = textColor,
+        ),
+    ) {
+        Text(text = buttonText)
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun LoginTextButtonPreview() {
+    ShoppingListTheme {
+        LoginTextButton(buttonText = stringResource(R.string.new_list_dialog_create_button_text))
+    }
+}
