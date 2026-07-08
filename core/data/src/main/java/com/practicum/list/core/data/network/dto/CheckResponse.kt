69859a48129a54267681a08c1e384da73d978fb5
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-07-08 15:17:49 +0400

    auth network + element tap fix

--- core/data/src/main/java/com/practicum/list/core/data/network/dto/CheckResponse.kt
+++ core/data/src/main/java/com/practicum/list/core/data/network/dto/CheckResponse.kt
@@ -5,6 +5,6 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class CheckResponse(
-    @field:Json(name = "is_valid")
+    @Json(name = "is_valid")
     val isValid: Boolean,
 )
--- core/data/src/main/java/com/practicum/list/core/data/network/dto/RegisterResponse.kt
+++ core/data/src/main/java/com/practicum/list/core/data/network/dto/RegisterResponse.kt
@@ -5,10 +5,10 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class RegisterResponse(
-    @field:Json(name = "user_id")
+    @Json(name = "user_id")
     val userId: Long,
-    @field:Json(name = "access_token")
+    @Json(name = "access_token")
     val accessToken: String,
-    @field:Json(name = "refresh_token")
+    @Json(name = "refresh_token")
     val refreshToken: String
 )
