545cb489dc8b73ad600449d56ed4befc9386edca
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-07-08 15:22:50 +0400

    element tap fix

--- core/design/src/main/java/com/practicum/list/core/components/cards/ShoppingListItem.kt
+++ core/design/src/main/java/com/practicum/list/core/components/cards/ShoppingListItem.kt
@@ -17,10 +17,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemColors
 import androidx.compose.material3.MaterialTheme
@@ -29,13 +27,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import com.practicum.list.core.components.buttons.RoundIconButton
 import com.practicum.list.core.components.buttons.ShoppingListActions
 import com.practicum.list.core.theme.Dimens.AnimationDuration
 import com.practicum.list.core.theme.Dimens.ListItemHeight
@@ -122,20 +119,13 @@ fun ShoppingListCell(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
             ) {
-                Box(
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(RoundedCornerShape(12.dp))
-                        .background(MaterialTheme.colorScheme.secondaryContainer),
-                    contentAlignment = Alignment.Center,
-                ) {
-                    Icon(
-                        painter = painterResource(iconResId),
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onSecondaryContainer,
-                        modifier = Modifier.size(24.dp),
-                    )
-                }
+                RoundIconButton(
+                    resId = iconResId,
+                    onClick = {},
+                    contentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                    containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                    enabled = false,
+                )
                 Text(
                     maxLines = 1,
                     text = text,
