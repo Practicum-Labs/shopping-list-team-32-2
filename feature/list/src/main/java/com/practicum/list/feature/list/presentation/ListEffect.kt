0bd2d0a92832f7a723bcbd2ac5ae2a4dfa019ed8
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-07-10 19:15:35 +0400

    intent + effect

--- feature/list/src/main/java/com/practicum/list/feature/list/presentation/ListEffect.kt
+++ feature/list/src/main/java/com/practicum/list/feature/list/presentation/ListEffect.kt
@@ -1,8 +1,17 @@
 package com.practicum.list.feature.list.presentation
 
+import com.practicum.list.core.common.domain.MeasureUnit
 import com.practicum.list.core.mvi.MviEffect
 
 sealed class ListEffect : MviEffect {
     data object NavigateToMain : ListEffect()
     data class ShowError(val message: String) : ListEffect()
+
+    data class ShoEditBottomSheet(val quantity: Float, val units: MeasureUnit ) : ListEffect()
+
+    data object ShowContextMenu : ListEffect()
+
+    data class ShowConfirmationDialog(val id: Long) : ListEffect()
+
+    data object ShowProductAddBottomSheet: ListEffect()
 }
--- feature/list/src/main/java/com/practicum/list/feature/list/presentation/ListIntent.kt
+++ feature/list/src/main/java/com/practicum/list/feature/list/presentation/ListIntent.kt
@@ -1,9 +1,47 @@
 package com.practicum.list.feature.list.presentation
 
+import android.icu.util.MeasureUnit
 import com.practicum.list.core.mvi.MviIntent
 
 sealed class ListIntent : MviIntent {
     data object BackClicked : ListIntent()
+
+    data object LoadList : ListIntent()
+
+    data class ConfirmDeleteListItems(val id: Long) : ListIntent()
+
+    data class UpdateQuantityAndUnits(val id: Long): ListIntent()
+
+    data object DeleteDialogDismissed: ListIntent()
+
+    data class DeleteDialogConfirmed(val listId: Long): ListIntent()
+
+    data object DeleteCheckedProductsDialogDismissed: ListIntent()
+
+    data class DeleteCheckedProductsDialpgConfirmed(val listId: Long): ListIntent()
+
+    data class UserCustomSortClicked(val listId: Long): ListIntent()
+
+    data class AlphabeticalSortClicked(val listId: Long): ListIntent()
+
+    data class ItemMoved(val productId: Long, val sortPosition: Int): ListIntent()
+
+    data object AddButtonClicked: ListIntent()
+
+    data class AddProductNameChanged(val name: String) : ListIntent()
+
+    data object AddProductPlusClicked : ListIntent()
+
+    data object AddProductMinusClicked: ListIntent()
+
+    data class AddProductQuantityChanged(val quantity: Float) : ListIntent()
+
+    data class AddProductUnitsChanged(val unit: MeasureUnit) : ListIntent()
+
+    data object AddProductConfirmed: ListIntent()
+
+
+
     data object AddProductClicked : ListIntent()
     data object OptionsMenuClicked : ListIntent()
     data class ToggleProductChecked(val productId: Long) : ListIntent()
